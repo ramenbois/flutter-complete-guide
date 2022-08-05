21d535bf2428b17994ea85fe7befa6de68b37e1c
Iqbal Ajie Wahyudin <iqbalajiewahyudin@gmail.com>
2022-08-05 08:28:57 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: ExampleScreen(),
+    );
+  }
+}
+
+class ExampleScreen extends StatelessWidget {
+  const ExampleScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Dummy Screen AppBar'),
+      ),
+      body: const Center(
+        child: Text('Dummy Screen Body'),
+      ),
+    );
+  }
+}
